1cab9f8c3eb0bc2cca05bd1c65fc57829cfbb8bb
IdeaSeeker <stroganov.n.36@gmail.com>
2020-04-18 22:34:00 +0300

    Обновление проекта

--- src/Riddler.kt
+++
@@ -1,8 +0,0 @@
-interface Riddler {
-    val length: Int
-    val maxDigit: Int
-
-    fun chooseNumber()
-    fun getCorrectAnswer(): String
-    fun check(attempt: String): Pair<Int, Int>
-}
--- src/Solver.kt
+++
@@ -1,7 +0,0 @@
-interface Solver {
-    val length: Int
-    val maxDigit: Int
-
-    fun makeAttempt(): String
-    fun parseResponse(response: Pair<Int, Int>)
-}
