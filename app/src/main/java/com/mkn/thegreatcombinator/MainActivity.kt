3de3006cad45c8598de26eb2ba2eec4358894a49
bagrorg <kuldanvit@gmail.com>
2020-05-08 00:38:32 +0300

    Добавлена реализация настроек: можно выставлять значения самому, значения сохраняются после выхода и они уже взаимодействуют с другими активити. Добавлен временный интерфейс в активити настроек.

--- app/src/main/java/com/mkn/thegreatcombinator/MainActivity.kt
+++ app/src/main/java/com/mkn/thegreatcombinator/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.mkn.thegreatcombinator
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,12 +11,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     var settingsLength: Int = 4
     var settingsMaxDigits: Int = 6
+    val APP_PREFERENCES_NAME = "Settings"
+    val APP_PREFERENCES_LENGTH = "Length"
+    val APP_PREFERENCES_MAX_DIGITS = "MaxDigits"
+    lateinit var pref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pref = getSharedPreferences(APP_PREFERENCES_NAME, Context.MODE_PRIVATE)
 
         toActivityRiddlerAI.setOnClickListener {
+            checkSettings()
             val intent = Intent(this, GameActivityRiddlerAI::class.java)
             val b = Bundle()
             b.putInt("maxDigits", settingsMaxDigits)
@@ -24,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         toActivitySolverAI.setOnClickListener {
+            checkSettings()
             val intent = Intent(this, GameActivitySolverAI::class.java)
             val b = Bundle()
             b.putInt("maxDigits", settingsMaxDigits)
@@ -41,5 +50,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, InfoActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun checkSettings() {
+        settingsLength = pref.getInt(APP_PREFERENCES_LENGTH, 4)
+        settingsMaxDigits = pref.getInt(APP_PREFERENCES_MAX_DIGITS, 6)
     }
 }
--- app/src/main/java/com/mkn/thegreatcombinator/SettingsActivity.kt
+++ app/src/main/java/com/mkn/thegreatcombinator/SettingsActivity.kt
@@ -1,13 +1,58 @@
 package com.mkn.thegreatcombinator
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+    val APP_PREFERENCES_NAME = "Settings"
+    val APP_PREFERENCES_LENGTH = "Length"
+    val APP_PREFERENCES_MAX_DIGITS = "MaxDigits"
+    lateinit var pref: SharedPreferences
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        
+        pref = getSharedPreferences(APP_PREFERENCES_NAME, Context.MODE_PRIVATE)
+        setButtons()
+
+
     }
+
+    private fun setButtons() {
+        for (i in 0 until (lenGroup.childCount)) {
+            val button: RadioButton = lenGroup.getChildAt(i) as RadioButton
+
+            if (button.text.toString().toInt() == pref.getInt(APP_PREFERENCES_LENGTH, 4)) {
+                button.isChecked = true
+            }
+
+            button.setOnClickListener{
+                val editor = pref.edit()
+                editor.remove(APP_PREFERENCES_LENGTH)
+                editor.putInt(APP_PREFERENCES_LENGTH, button.text.toString().toInt())
+                editor.apply()
+            }
+        }
+
+        for (i in 0 until (mDigGroup.childCount)) {
+            val button: RadioButton = mDigGroup.getChildAt(i) as RadioButton
+
+            if (button.text.toString().toInt() == pref.getInt(APP_PREFERENCES_MAX_DIGITS, 6)) {
+                button.isChecked = true
+            }
+
+            button.setOnClickListener{
+                val editor = pref.edit()
+                editor.remove(APP_PREFERENCES_MAX_DIGITS)
+                editor.putInt(APP_PREFERENCES_MAX_DIGITS, button.text.toString().toInt())
+                editor.apply()
+            }
+        }
+    }
+
 }
